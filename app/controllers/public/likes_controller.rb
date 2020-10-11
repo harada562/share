897180b032f8435cd3d1ef512e957d37e5c20920
vagrant <vagrant@localhost.localdomain>
2020-10-11 08:54:49 +0000

    いいね ajax

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,14 +1,16 @@
 class Public::LikesController < ApplicationController
   before_action :authenticate
   def create
-    @like = current_customer.likes.create(place_id: params[:place_id])
-    redirect_back(fallback_location: root_path)
+    @place = Place.find(params[:place_id])
+    @like = current_customer.likes.build(place_id: params[:place_id])
+    @like.save
+    # binding.pry
   end
 
   def destroy
+    @place = Place.find(params[:place_id])
     @like = Like.find_by(place_id: params[:place_id], customer_id: current_customer.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
   end
 
   private
