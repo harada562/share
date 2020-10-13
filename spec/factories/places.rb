150adfe26ca8f7fbb0a9746af8ffb108268f678b
vagrant <vagrant@localhost.localdomain>
2020-10-13 06:41:21 +0000

    systemテスト作成

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :place do
-    place_name { "testplace" }
-    address { "東京都" }
     association :customer
+    association :genre
+    sequence(:place_name) { |n| "カフェ#{n}" }
+    sequence(:address) { |n| "東京都新宿区#{n}" }
+    sequence(:number) { |n| "123456789#{n}" }
+    sequence(:place_url) { |n| "placeshare.work#{n}" }
+    sequence(:budget) { |n| "1000#{n}" }
+    sequence(:detail) { |n| "静かなカフェです#{n}" }
   end
 end
