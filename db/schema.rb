f27e446454f55f063b18131df7f72e6574fd9444
vagrant <vagrant@localhost.localdomain>
2020-10-13 12:13:28 +0000

    不要なファイルとカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_065111) do
+ActiveRecord::Schema.define(version: 2020_10_13_121221) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_10_05_065111) do
     t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ImageId"
     t.string "nick_name"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["nick_name"], name: "index_customers_on_nick_name", unique: true
@@ -47,7 +48,6 @@ ActiveRecord::Schema.define(version: 2020_10_05_065111) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "customer_id"
   end
 
   create_table "groups", force: :cascade do |t|
