824f639fdd092d37d35a4b012ddf2b57fddffb44
vagrant <vagrant@localhost.localdomain>
2020-10-20 00:04:55 +0000

    レスポンシブデザイン

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,9 +24,11 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
-
   # スライドショーデプロイのため
   config.assets.js_compressor = Uglifier.new(harmony: true)
+
+  # スライドショーデプロイのため
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
