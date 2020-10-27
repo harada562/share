646f34a3ba6eddb1440cd72743f81bf07d11fa50
vagrant <vagrant@localhost.localdomain>
2020-10-27 00:59:11 +0000

    デザイン変更

--- app/assets/javascripts/public/customer_show.js
+++ app/assets/javascripts/public/customer_show.js
@@ -1,10 +1,10 @@
 var mySwiper = new Swiper('.swiper-container', {
-	autoplay: {
-		delay: 3000,
-		stopOnLastSlide: false,
-		disableOnInteraction: false,
-		reverseDirection: false
-	},
+	// autoplay: {
+	// 	delay: 3000,
+	// 	stopOnLastSlide: false,
+	// 	disableOnInteraction: false,
+	// 	reverseDirection: false
+	// },
 	loop: true,
 	pagination: {
 		el: '.swiper-pagination',
