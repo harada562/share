f33466cb9f0d411362cdb7f08e37318078caf9de
vagrant <vagrant@localhost.localdomain>
2020-10-27 00:37:35 +0000

    swiper追加

---
+++ app/assets/javascripts/public/customer_show.js
@@ -0,0 +1,14 @@
+var mySwiper = new Swiper('.swiper-container', {
+	autoplay: {
+		delay: 3000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+		reverseDirection: false
+	},
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true
+	}
+});
