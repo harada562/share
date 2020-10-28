eb59dc54ca8fdf8e31880d5bbb7bbc4709b44513
vagrant <vagrant@localhost.localdomain>
2020-10-28 01:35:31 +0000

    レスポンシブデザイン

--- spec/models/place_image_spec.rb
+++ spec/models/place_image_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe PlaceImage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	context "データが正しく保存される" do
+		before do
+			# customer作成
+    		@customer = create(:customer)
+			# place 作成
+			@place = create(:place)
+		end
+		it "全て入力してあるので保存される" do
+			# place_imagesの作成
+			@place_image = PlaceImage.new
+			@place_image.image_id = "jjj.png"
+			@place_image.place_id = @place.id
+			@place_image.save
+			expect(@place_image).to be_valid
+		end
+	end
+	context "データが保存されない" do
+		before do
+			# customer作成
+    		@customer = create(:customer)
+			# place 作成
+			@place = create(:place)
+		end
+		it "空白があるので保存されない" do
+			# place_imagesの作成
+			@place_image = PlaceImage.new
+			@place_image.image_id = "ll.png"
+			@place_image.place_id = ""
+			@place_image.save
+			expect(@place_image.errors[:place_id]).to include("を入力してください")
+		end
+	end
 end
